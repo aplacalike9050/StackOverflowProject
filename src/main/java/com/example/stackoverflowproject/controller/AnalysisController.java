648a212731db4ee3aafc7283796cb7db691373f7
aplacalike9050 <aplacalike9050@gmail.com>
2025-12-11 00:58:40 +0800

    添加前端代码：index.html

--- src/main/java/com/example/stackoverflowproject/controller/AnalysisController.java
+++ src/main/java/com/example/stackoverflowproject/controller/AnalysisController.java
@@ -6,16 +6,13 @@ import com.example.stackoverflowproject.dto.SolvabilityDTO;
 import com.example.stackoverflowproject.dto.TopicTrendDTO;
 import com.example.stackoverflowproject.service.AnalysisService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/analysis")
-
+@CrossOrigin(origins = "*")
 public class AnalysisController {
 
     private final AnalysisService analysisService;
